90ca17c072e2c300380648874bc39e99cba26d91
Igor <imoncap.dev@gmail.com>
2023-01-10 21:20:48 -0300

    implementando dados no localStorage

--- js/main.js
+++ js/main.js
@@ -1,23 +1,66 @@
 const form = document.getElementById("novoItem")
 const lista = document.getElementById("lista")
+const itens = JSON.parse(localStorage.getItem("itens")) || []
+
+itens.forEach( (elemento) => {
+    criaElemento(elemento)
+} )
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
 
-    criaElemento(evento.target.elements["nome"].value, evento.target.elements["quantidade"].value)
+    const nome = evento.target.elements["nome"]
+    const quantidade = evento.target.elements["quantidade"]
+
+    const existe = itens.find(elemento => elemento.nome === nome.value)
+
+    const itemAtual = {
+        "nome": nome.value,
+        "quantidade": quantidade.value
+    }
+
+    if (existe) {
+        itemAtual.id = existe.id
+
+        atualizaElemento(itemAtual)
+
+        itens[existe.id] = itemAtual
+    } else {
+        itemAtual.id = itens.length
+
+        criaElemento(itemAtual)
+
+        itens.push(itemAtual)
+    }
+
+    
+
+    
+
+    localStorage.setItem("itens", JSON.stringify(itens))
+
+    //Esvazia formulário após o submit.
+    nome.value = ""
+    quantidade.value = ""
 })
 
-function criaElemento(nome, quantidade) {
+//Cria nova linha no HTML contendo Nome e Quantidade informados no formulário
+function criaElemento(item) {
     const novoItem = document.createElement('li')
     novoItem.classList.add("item")
 
     const numeroItem = document.createElement('strong')
-    numeroItem.innerHTML = quantidade
+    numeroItem.innerHTML = item.quantidade
+    numeroItem.dataset.id = item.id
 
     novoItem.appendChild(numeroItem)
-    novoItem.innerHTML += nome
-
-    const lista = document.getElementById("lista")
+    novoItem.innerHTML += item.nome
 
     lista.appendChild(novoItem)
+
+
+}
+
+function atualizaElemento(item) {
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
 }
